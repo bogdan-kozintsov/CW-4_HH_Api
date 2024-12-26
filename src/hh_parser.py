d8e3fad1cd74406a1679fc43184ddbdb518b757e
bogdan.kozintsov <bogdan.kozintsov@gmail.com>
2024-12-27 09:05:02 +1100

    fix class Vacancy updated def to_dict

--- src/hh_parser.py
+++ src/hh_parser.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 from abc import ABC, abstractmethod
 
@@ -20,7 +22,7 @@ class HH:
     def __init__(self):
         self.url = 'https://api.hh.ru/vacancies'
         self.headers = {'User-Agent': 'HH-User-Agent'}
-        self.params = {'text': '', 'page': 0, 'per_page': 100}
+        self.params = {'text': '', 'page': 0, 'per_page': 100, 'area': 113, 'salary': 100000, 'only_with_salary': True}
         self.vacancies = []
 
     def load_vacancies(self, keyword):
@@ -35,20 +37,65 @@ class HH:
 class Vacancy:
     """Класс для представления вакансии"""
 
-    def __init__(self, name, alternate_url):
+    def __init__(self, name, alternate_url, area, salary, currency, employer):
         self.name = name
         self.alternate_url = alternate_url
+        self.area = area['name']
+        self.salary_from = salary.get('from')
+        self.salary_to = salary.get('to')
+        self.salary = f'{self.salary_from} - {self.salary_to} {currency}' if self.salary_from and self.salary_to else 'Не указано'
+        self.employer = employer['name']
+
+    def to_dict(self):
+        return {
+            'Вакансия': self.name,
+            'Работодатель': self.employer,
+            'Город': self.area,
+            'Зарплата': self.salary,
+            'Ссылка на вакансию': self.alternate_url
+        }
 
     def __repr__(self):
-        return f'\nНазвание: {self.name}, URL: {self.alternate_url}'
+        return (f'Название: {self.name}\n'
+                f'URL: {self.alternate_url}\n'
+                f'Area: {self.area}\n'
+                f'Salary: {self.salary}\n'
+                f'Employer: {self.employer}')
+
+
+class JSONFileSaver:
+    """Класс для сохранения вакансий в JSON-файл"""
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def save(self, vacancies):
+        with open(self.file_path, 'w', encoding='utf-8') as f:
+            json.dump(vacancies, f, ensure_ascii=False, indent=4)
+
+    def get(self):
+        pass
+
+    def delete(self):
+        pass
 
 
 if __name__ == '__main__':
     hh = HH()
-    hh.load_vacancies('Python')
+    sever = JSONFileSaver('C:/Users/denis.shtepa/bogdan.kozintsov/CodePractice/CW-4_HH_Api/data/vacancies.json')
+    hh.load_vacancies('водитель')
 
     vacancy_objects = []
     for vacancy in hh.vacancies:
-        vacancy_objects.append(Vacancy(vacancy["name"], vacancy["alternate_url"]))
+        vacancy_objects.append(Vacancy(vacancy["name"],
+                                       vacancy["alternate_url"],
+                                       vacancy["area"],
+                                       vacancy["salary"],
+                                       vacancy["salary"]["currency"],
+                                       vacancy["employer"]))
 
-    print(vacancy_objects)
+    vacancy_dicts = []
+    for vacancy_object in vacancy_objects:
+        vacancy_dicts.append(vacancy_object.to_dict())
+
+    sever.save(vacancy_dicts)
